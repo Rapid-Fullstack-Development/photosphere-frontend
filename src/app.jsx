e1b30d9fa0cbae5a9d35acef47e53e1816c008c9
Ashley Davis <ashley@codecapers.com.au>
2022-10-23 10:36:05 +1000

    Refactored the app component down to a "gallery page" component in preparation for adding multiple pages.

--- src/app.jsx
+++ src/app.jsx
@@ -1,37 +1,11 @@
 import React from "react";
-import axios from "axios";
-import { Gallery } from "./lib/gallery";
-
-const BASE_URL = process.env.BASE_URL;
-if (!BASE_URL) {
-    throw new Error(`Set BASE_URL environment variable to the URL for the Photosphere backend.`);
-}
-
-console.log(`Expecting backend at ${BASE_URL}.`);
+import { GalleryPage } from "./pages/gallery";
 
 export class App extends React.Component {
 
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            items: [],
-        };
-    }
-
-    async componentDidMount() {
-        const response = await axios.get(`${BASE_URL}/assets`);
-        this.setState({
-            items: response.data.assets,
-        });    
-    }
-
     render() {
         return (
-            <Gallery 
-                items={this.state.items}                
-                baseUrl={BASE_URL}
-                />
+            <GalleryPage /> 
         );
     }
 }
