bcdf2651f2aab70b7c325f6c934081f11c46d61c
Ashley Davis <ashley@codecapers.com.au>
2022-10-23 10:36:06 +1000

    Added an input element for uploading files with an event handler to log the details of the event.

--- src/pages/upload.jsx
+++ src/pages/upload.jsx
@@ -5,7 +5,15 @@ export class UploadPage extends React.Component {
     render() {
         return (
             <div>
-                Placeholder for the upload page.
+                <p>Click the button and choose files to upload</p>
+                <input
+                    type="file"
+                    multiple={true}
+                    accept="image/*"
+                    onChange={event => {
+                        console.log(event);
+                    }}
+                    />
             </div>
         );
     }
