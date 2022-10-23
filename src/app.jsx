7d3fe16e6747a50ad42e92bfd2f23661a42dd113
Ashley Davis <ashley@codecapers.com.au>
2022-10-23 10:36:05 +1000

    Requesting assets from the backend and logging to the console.

--- src/app.jsx
+++ src/app.jsx
@@ -1,3 +1,16 @@
-export function App() {
-    return <h1>Hello computer!</h1>;
+import React from "react";
+import axios from "axios";
+
+export class App extends React.Component {
+
+    async componentDidMount() {
+        const response = await axios.get(`http://localhost:3000/assets`);
+        console.log(response.data);
+    }
+
+    render() {
+        return (
+            <h1>Hello computer!</h1>
+        );
+    }
 }
