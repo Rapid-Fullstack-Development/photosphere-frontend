abe0fceccf2dfcc56d88294f547dbcfb136d4d59
Ashley Davis <ashley@codecapers.com.au>
2022-10-23 10:36:05 +1000

    Passing assets into the photo gallery.

--- src/app.jsx
+++ src/app.jsx
@@ -1,16 +1,32 @@
 import React from "react";
 import axios from "axios";
+import { Gallery } from "./lib/gallery";
+
+const BASE_URL = `http://localhost:3000`;
 
 export class App extends React.Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            items: [],
+        };
+    }
+
     async componentDidMount() {
-        const response = await axios.get(`http://localhost:3000/assets`);
-        console.log(response.data);
+        const response = await axios.get(`${BASE_URL}/assets`);
+        this.setState({
+            items: response.data.assets,
+        });    
     }
 
     render() {
         return (
-            <h1>Hello computer!</h1>
+            <Gallery 
+                items={this.state.items}                
+                baseUrl={BASE_URL}
+                />
         );
     }
 }
